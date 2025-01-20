91fd938264f8ab4a4f907f58d5b6650afb5aa01b
Shriniwas Bhardwaz <shrini.kvpy3@gmail.com>
2025-01-20 08:48:59 +0530

    Tic Tac Toe design - human game

--- src/LowLevelDesign/DesignTicTacToeMain/Main.java
+++ src/LowLevelDesign/DesignTicTacToeMain/Main.java
@@ -5,7 +5,7 @@ package LowLevelDesign.DesignTicTacToeMain;
 public class Main {
     public static void main(String[] args) {
         GameController controller = GameController.getInstance();
-        int gameId = controller.createGame(3, GameMode.HUMAN_VS_AI,2);
+        int gameId = controller.createGame(3, GameMode.HUMAN_VS_HOME,2);
         controller.startGame(gameId);
 
     }
